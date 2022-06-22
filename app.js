32927ff775300e79131a35d6ce0c4757f4fe2c52
Gaurav Mutha <gauravmutha1412@gmail.com>
2022-06-22 19:31:30 +0530

    Introduces level 3 security - Hashing

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require("dotenv").config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("mongoose")
-const encrypt=require("mongoose-encryption")
+const mongoose = require("mongoose");
+const md5 = require("md5");
 
 const app = express();
 
@@ -30,8 +30,6 @@ const userSchema= new mongoose.Schema({
     }
 })
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
-
 const User= new mongoose.model("User",userSchema)
 
 app.get("/",function(req,res){
@@ -49,7 +47,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newuser= new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     newuser.save(function(err){
@@ -64,7 +62,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
 
     User.findOne({email:username},function(err,founduser){
         if(!err){
